d2383d0f2696d0a791de9ff4833081ef1d1f4f9d
luisibarra06 <42873966+luisibarra06@users.noreply.github.com>
2018-09-10 10:15:41 -0700

    Add files via upload

---
+++ nums1.py
@@ -0,0 +1,7 @@
+# nums1.py lai
+
+x = 1000000000000000    # int
+y = 2.7878732323  # float
+z = 1j   # complex
+print (" integer, float, complex" ) 
+print (x, y, z)
---
+++ var2.py
@@ -0,0 +1,8 @@
+# var2.py lai
+a = 10 # integer (int)
+b = -5 # integer (int)
+s2 = 1.414 # float
+s = "This is a string of characters" # string
+s3 = 'How about this' # string
+print (a,b,s2,s,s3)
+
