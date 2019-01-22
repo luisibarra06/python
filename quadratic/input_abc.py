6f1038c73bcba6ddd43441ccd73b0904df132b17
luisibarra06 <42873966+luisibarra06@users.noreply.github.com>
2019-01-22 08:20:38 -0800

    Add files via upload

---
+++ quadratic/input_abc.py
@@ -0,0 +1,15 @@
+from class_quadratic import *
+
+def main():
+    print("Input a,b and c from an equation ax^2 + bx + c : ")
+
+
+
+    p1 = QuadraticEquation(1,0,-9)
+    x1 = p1.x1()
+    x2 = p1.x2()
+    print ("Discriminant = ",p1.Discriminant())
+    print ("x1 = ",x1," x2 = ",x2)
+
+if __name__ == "__main__":
+    main()
